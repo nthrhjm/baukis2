01b35abd8fad8b684e1ca1a58d0656fdc369bfa2
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-04-03 14:11:06 +0900

    update spec to services/admin

--- spec/services/admin/authenticator_spec.rb
+++ spec/services/admin/authenticator_spec.rb
@@ -1,5 +1,18 @@
 require "rails_helper"
 
 describe Admin::Authenticator do
-
+  describe "#authenticate" do
+    example "正しいパスワードならtrueを返す" do
+      a = build(:administrator)
+      expect(Admin::Authenticator.new(a).authenticate("pw")).to be_truthy
+    end
+    example "誤ったパスワードならfalseを返す" do
+      a = build(:administrator)
+      expect(Admin::Authenticator.new(a).authenticate("xy")).to be_falsey
+    end
+    example "停止フラグが立っていてもtrueを返す" do
+      a = build(:administrator, suspended: true)
+      expect(Admin::Authenticator.new(a).authenticate("pw")).to be_truthy
+    end
+  end
 end

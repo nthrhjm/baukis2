e6866f243fd33c14fb45871764294164016175a6
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-04-07 12:54:21 +0900

    code formatting

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,5 +1,6 @@
 class Admin::Base < ApplicationController
   before_action :authorize
+
   private def current_administrator
     if session[:administrator_id]
       @current_administrator ||= Administrator.find_by(id: session[:administrator_id])

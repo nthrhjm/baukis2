d83cadeffae30654cb0863e47ba7d0bca1bfc5cd
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-04-11 23:41:25 +0900

    add spec to staff_member model family_name, given_name

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -55,5 +55,13 @@ RSpec.describe StaffMember, type: :model do
       member2 = build(:staff_member, email: member1.email)
       expect(member2).not_to be_valid
     end
+    example "アルファベット表記のfamily_nameは有効" do
+      member = build(:staff_member, family_name: "Smith")
+      expect(member).to be_valid
+    end
+    example "記号を含むfamily_nameは無効" do
+      member = build(:staff_member, family_name: "試験⭐️")
+      expect(member).not_to be_valid
+    end
   end
 end

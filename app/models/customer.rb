7e67b114b130122682c154011ade8cd710415c83
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-04-19 23:33:09 +0900

    タイポを修正 (string -> String)

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   has_one :work_address, dependent: :destroy
 
   def password=(raw_password)
-    if raw_password.kind_of?(string)
+    if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
     elsif raw_password.nil?
       self.hashed_password = nil

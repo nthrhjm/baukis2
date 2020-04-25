6949844dd025dac47178f5f15a5b95c413f8d558
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-04-25 17:04:46 +0900

    顧客情報の新規登録/編集機能を実装

--- app/controllers/staff/customers_controller.rb
+++ app/controllers/staff/customers_controller.rb
@@ -14,4 +14,28 @@ class Staff::CustomersController < Staff::Base
   def edit
     @customer_form = Staff::CustomerForm.new(Customer.find(params[:id]))
   end
+
+  def create
+    @customer_form = Staff::CustomerForm.new
+    @customer_form.assign_attributes(params[:form])
+    if @customer_form.save
+      flash.notice = "顧客情報を保存しました。"
+      redirect_to action: "index"
+    else
+      flash.now.alert = "入力に誤りがあります。"
+      render action: "new"
+    end
+  end
+
+  def update
+    @customer_form = Staff::CustomerForm.new(Customer.find(params[:id]))
+    @customer_form.assign_attributes(params[:form])
+    if @customer_form.save
+      flash.notice = "顧客情報を更新しました。"
+      redirect_to action: "index"
+    else
+      flash.new.alert = "入力に誤りがあります。"
+      render action: "edit"
+    end
+  end
 end

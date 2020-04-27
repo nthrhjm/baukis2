4727e84f865a5d588bc62cb5fd4b8fd16b9db477
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-04-27 15:47:22 +0900

    Password に対する validation を 別クラスに書き出し、customer, staff_member, administrator で include するように変更

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ApplicationRecord
   include PersonalNameHolder
   include EmailHolder
+  include PasswordHolder
 
   has_one :home_address, dependent: :destroy, autosave: true
   has_one :work_address, dependent: :destroy, autosave: true
@@ -11,12 +12,4 @@ class Customer < ApplicationRecord
       allow_blank: true,
       after: Date.new(1900, 1, 1)
   }
-
-  def password=(raw_password)
-    if raw_password.kind_of?(String)
-      self.hashed_password = BCrypt::Password.create(raw_password)
-    elsif raw_password.nil?
-      self.hashed_password = nil
-    end
-  end
 end
--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,6 +1,7 @@
 class StaffMember < ApplicationRecord
   include EmailHolder
   include PersonalNameHolder
+  include PasswordHolder
 
   has_many :events, class_name: "StaffEvent", dependent: :destroy
 
@@ -8,7 +9,7 @@ class StaffMember < ApplicationRecord
 
   validates :start_date, presence: true, date: {
       after_or_equal_to: Date.new(2000, 1, 1),
-      before: -> (obh) { 1.year.from_now.to_date },
+      before: -> (obj) { 1.year.from_now.to_date },
       allow_blank: true
   }
   validates :end_date, date: {
@@ -16,14 +17,6 @@ class StaffMember < ApplicationRecord
       before: ->(obj) { 1.year.from_now.to_date },
       allow_blank: true
   }
-
-  def password=(raw_password)
-    if raw_password.kind_of?(String)
-      self.hashed_password = BCrypt::Password.create(raw_password)
-    elsif raw_password.nil?
-      self.hashed_password = nil
-    end
-  end
 
   def active?
     !suspended? && start_date <= Date.today && (end_date.nil? || end_date > Date.today)
